38e6201a5bcd5aad3a2fcf39c90d4f078b110740
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-03-13 07:43:20 -0400

    creating database folder while creating docker image

--- whoismrsaemir/entrypoint.sh
+++ whoismrsaemir/entrypoint.sh
@@ -2,6 +2,7 @@
 
 crontab ./whoismrsaemir/cronjobs
 service cron start
+mkdir -p ./whoismrsaemir/database
 python3 ./whoismrsaemir/manage.py makemigrations
 python3 ./whoismrsaemir/manage.py migrate
 python3 ./whoismrsaemir/manage.py runserver 0.0.0.0:8000
