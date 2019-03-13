e5b652bd04b129b0192fe72fb9a46c2dd9bfb555
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-03-13 03:04:38 -0400

    updted entrypoint.sh

--- whoismrsaemir/entrypoint.sh
+++ whoismrsaemir/entrypoint.sh
@@ -4,5 +4,4 @@ crontab ./whoismrsaemir/cronjobs
 service cron start
 python3 ./whoismrsaemir/manage.py makemigrations
 python3 ./whoismrsaemir/manage.py migrate
-python3 ./whoismrsaemir/manage.py createsuperuser
 python3 ./whoismrsaemir/manage.py runserver 0.0.0.0:8000
